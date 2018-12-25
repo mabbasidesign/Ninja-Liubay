131a7964f20556a910c4d46a23721d69c11c783c
unknown <mabbasidesign@yahoo.com>
2018-12-25 11:26:52 +0330

    add ninjas list

--- ninja-app/src/App.js
+++ ninja-app/src/App.js
@@ -1,21 +1,50 @@
+// import React, { Component } from 'react';
+// import Ninjas from './ninjas';
+
+// class App extends Component {
+//   render() {
+//     state = {
+//       ninjas: [
+//         { id:1, name:"Ryan", age:30, belt:"Blak" },
+//         { id:2, name:"Jorj", age:25, belt:"Beown" },
+//         { id:3, name:"David", age:34, belt:"Pink" },
+//       ]
+//      }
+
+//     // const {ninjas} = this.state;
+
+//     return (
+//       <div className="App">
+//         <Ninjas ninjas={this.state.ninjas} />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
 import React, { Component } from 'react';
 import Ninjas from './ninjas';
 
+
 class App extends Component {
+ state = {
+  ninjas: [
+    { id:1, name:"Ryan", age:30, belt:"Blak" },
+    { id:2, name:"Jorj", age:25, belt:"Beown" },
+    { id:3, name:"David", age:34, belt:"Pink" },
+  ]
+ }
+
+  handleDelete = (id) => {
+    let ninjas = this.state.ninjas.filter(ninja => ninja.id !== id);
+    this.setState({ ninjas });
+  }
+  
   render() {
-    state = {
-      ninjas: [
-        { id:1, name:"Ryan", age:30, belt:"Blak" },
-        { id:2, name:"Jorj", age:25, belt:"Beown" },
-        { id:3, name:"David", age:34, belt:"Pink" },
-      ]
-    }
-
-    const {ninjas} = this.state;
-
     return (
-      <div className="App">
-        <Ninjas ninjas={ninjas} />
+      <div>
+        <Ninjas ninjas={this.state.ninjas} deleteNinja={this.handleDelete} />
       </div>
     );
   }
--- ninja-app/src/ninjas.jsx
+++ ninja-app/src/ninjas.jsx
@@ -4,11 +4,20 @@ class Ninjas extends Component {
 
     render() {
 
-        const
+    const ninjaList = this.props.ninjas.map(ninja => {
+        return(
+            <div key={ninja.id} className="ninja" >
+                <div>Name: {ninja.name}</div>
+                <div>Age: {ninja.age}</div>
+                <div>Belt: {ninja.belt}</div>
+                <button onClick={() => this.props.deleteNinja(ninja.id)}>Delete</button>
+            </div>
+        )
+    });
 
         return ( 
             <div>
-                
+                {ninjaList}
             </div>
          );
     }
